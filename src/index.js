6147539c777a0f6706eb1b1760144c577cc1bdd9
Eric Pcholinski <ericpcho@gmail.com>
2018-05-16 15:33:17 -0700

    properly setup redux, added components, reducers, actions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { reducer } from './reducers/reducer.js';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/reducer.js
@@ -0,0 +1,9 @@
+import * as actions from '../actions/actions.js'
+
+const initialState = {
+    view: 'home'
+}
+
+export const reducer = (state = initialState, action) => {
+    return state
+}
