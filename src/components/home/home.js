e6581a838806e25b3e160f568a9e6f39fba0a137
Eric Pcholinski <ericpcho@gmail.com>
2018-06-25 12:05:40 -0700

    added new spreads to thanks page, social media images

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -194,21 +194,23 @@ export class Home extends React.Component {
                     <input type="hidden" name="_redirect" value="https://dcanatomy.insighteditions.com/thanks"/>
                     <button onClick={() => this.changeView("question7")} id="form-submit" className="form-submit" type="submit" disabled>Submit</button>
                     </form>
-            </section>
-        }
-
-        if (this.props.view === 'question7') {
-            pageView = <section className="container">
-                    <div className="title-logo"><img className="anatomy" src={anatomy}/></div>
-                    <h2>Congratulations! You are one of the very few to access this page.</h2>
-                    <h2>As a reward, here are exclusive srpeads from DC Comics: Anatomy of a Metahuman</h2>
-                    <h2>Available September 2018</h2>
-                    <div className="spread1"> </div>
-                    <div className="spread2"> </div>
                     <a href="http://insighteditions.com" className="ie-logo"> </a>
             <a href="https://www.dccomics.com" className="wb-logo"><span className="legal" >All related characters and elements © & ™ DC Comics. (s18)</span></a>
             </section>
         }
+
+        // if (this.props.view === 'question7') {
+        //     pageView = <section className="container">
+        //             <div className="title-logo"><img className="anatomy" src={anatomy}/></div>
+        //             <h2>Congratulations! You are one of the very few to access this page.</h2>
+        //             <h2>As a reward, here are exclusive srpeads from DC Comics: Anatomy of a Metahuman</h2>
+        //             <h2>Available September 2018</h2>
+        //             <div className="spread1"> </div>
+        //             <div className="spread2"> </div>
+        //             <a href="http://insighteditions.com" className="ie-logo"> </a>
+        //     <a href="https://www.dccomics.com" className="wb-logo"><span className="legal" >All related characters and elements © & ™ DC Comics. (s18)</span></a>
+        //     </section>
+        // }
         
         if (this.props.view === "incorrect"){
             pageView = <section className="container">
--- src/components/thanks/thanks.js
+++ src/components/thanks/thanks.js
@@ -10,10 +10,18 @@ export class Thanks extends React.Component {
 <section className="container">
                     <div className="title-logo"><img className="anatomy" src={anatomy}/></div>
                     <h2>Congratulations! You are one of the very few to access this page.</h2>
-                    <h2>As a reward, here are exclusive srpeads from DC Comics: Anatomy of a Metahuman</h2>
+                    <h2>As a reward, here are exclusive spreads from DC Comics: Anatomy of a Metahuman</h2>
                     <h2>Available September 2018</h2>
                     <div className="spread1"> </div>
                     <div className="spread2"> </div>
+                    <div className="spread3"> </div>
+                    <div className="spread4"> </div>
+                    <h2 className="social-text">Want to learn more? Follow Insight Editions on social media </h2>
+                    <ul className="social-media">
+                        <li className="instagram"></li>
+                        <li className="facebook"></li>
+                        <li className="twitter"></li>
+                    </ul>
                     <a href="http://insighteditions.com" className="ie-logo"> </a>
             <a href="https://www.dccomics.com" className="wb-logo"><span className="legal" >All related characters and elements © & ™ DC Comics. (s18)</span></a>
             </section>
